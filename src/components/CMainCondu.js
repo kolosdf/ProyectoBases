16f099c5c074a89066d440ca947183f801052dd7
seibern <melissa.fuentes@correounivalle.edu.co>
2019-04-04 02:57:02 -0500

    añadiendo las paginas que permiten el registro

---
+++ src/components/CMainCondu.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import { Grid,Button,Image } from 'semantic-ui-react'
+import { withRouter } from 'react-router-dom';
+import ImgDispo from './../images/TaxiDisponibleICON.png';
+import ImgService from './../images/TaxiServicioICON.png';
+import ImgNoService from './../images/TaxiNoServicioICON.png';
+import ImgMap from './../images/MapaICON.png';
+import ImgTaxi from './../images/TaxiICON.png';
+import ImgPreferencies from './../images/PreferenciasICON.png';
+import ImgExit from './../images/SalirICON.png';
+import ImgAsist from './../images/AsistenciaICON.png';
+import ImgConduTag from './../images/ConduTag.png';
+import ImgLogo from './../images/logoYellow.png';
+
+const style = {
+    margin: '0.5em',
+    paddingLeft: 0,    
+    listStyle: 'none',
+    backgroundColor: '#FFCC00'
+};
+
+const styleButton = {
+    color: '#FFFFFF',
+    backgroundColor: '#FFCC00'
+};
+
+const styleWord = {
+    font: 'Open Sans',
+    fontstyle: 'oblique'
+};
+
+class MainCondu extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.handleClickUser = this.handleClickUser.bind(this)        
+        this.handleClickConduct = this.handleClickConduct.bind(this)
+    }
+
+    handleClickUser(event){
+        alert('Usuario')
+    }
+
+    handleClickConduct(event){
+        alert('Conductor')
+    }
+
+    render(){
+        return(
+            <Grid centered columns={3}  relaxed='very' style={style}> 
+                <Grid.Row>                    
+                    <Grid.Column floated='left'>
+                        <Image src={ImgConduTag} />
+                    </Grid.Column>
+                    <Grid.Column floated='right'>
+                        <Image src={ImgLogo} />
+                    </Grid.Column>
+                </Grid.Row>
+
+                <Grid.Row>
+                    <Grid.Column stretched>
+                        <Grid.Row>
+                            <Button compact style={styleButton}>
+                                <Image src={ImgNoService} />                            
+                            </Button>
+                        </Grid.Row>                    
+                        <Grid.Row>
+                            <Button compact style={styleButton}>
+                                <Image src={ImgPreferencies} />                            
+                            </Button>
+                        </Grid.Row>               
+                    </Grid.Column>
+
+                    <Grid.Column stretched>
+                        <Grid.Row>
+                            <Button compact style={styleButton}>
+                                <Image src={ImgMap} />                            
+                            </Button>
+                        </Grid.Row>                    
+                        <Grid.Row>
+                            <Button href='/' compact style={styleButton}>
+                                <Image src={ImgExit} />                            
+                            </Button>
+                        </Grid.Row>               
+                    </Grid.Column>
+
+                    <Grid.Column stretched>
+                        <Grid.Row>
+                            <Button compact style={styleButton}>
+                                <Image src={ImgTaxi} />                            
+                            </Button>
+                        </Grid.Row>                    
+                        <Grid.Row>
+                            <Button compact style={styleButton}>
+                                <Image src={ImgAsist} />                            
+                            </Button>
+                        </Grid.Row>               
+                    </Grid.Column>
+                </Grid.Row>                
+            </Grid>
+        );
+    }
+}
+
+export default withRouter(MainCondu);
