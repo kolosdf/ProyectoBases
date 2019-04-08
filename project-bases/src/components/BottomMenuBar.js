0bde6b76aea13cce78fda14f05aabc5890bf69fc
Shinku24 <juan.felipe.gil@correounivalle.edu.co>
2019-04-07 23:13:43 -0500

    Frontend: Added modal component for up and down menu options

--- project-bases/src/components/BottomMenuBar.js
+++ project-bases/src/components/BottomMenuBar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Grid,Image,List } from 'semantic-ui-react'
+import { Grid,Image,List,Modal,Button } from 'semantic-ui-react'
 import image from './../images/logoWhite.png'
 
 const style = {
@@ -18,51 +18,87 @@ export default class BottomMenu extends React.Component{
 
         this.state = {
             aboutUsM: 'Empresa que presta un servicio para pedir servicio de taxi de forma rapida y eficiente, que facilita el pago del servicio',
-            ourServicesM: 'Servicios: \n-Pedir taxi\n-Aceptar una solicitud de presta servicio de taxi',
             reviewM: 'No hay reseñas',
-            helpM: 'Juan felipe Gil - juan.felipe.gil@correounivalle.edu.co\nMelissa Fuentes - melissa.fuentes@correounivalle.edu.co',
+            modalSOpen: false,
+            modalAOpen: false,
         };
-
-        this.handleClickAboutUs = this.handleClickAboutUs.bind(this);
-        this.handleClickOurServM= this.handleClickOurServM.bind(this);
-        this.handleClickReviewM = this.handleClickReviewM.bind(this);
-        this.handleClickHelpM = this.handleClickHelpM.bind(this);
     }
 
-    handleClickAboutUs(){
-        alert(this.state.aboutUsM);
-    }
-
-    handleClickOurServM(){
-        alert(this.state.ourServicesM);
-    }
-
-    handleClickReviewM(){
-        alert(this.state.reviewM);
-    }
-
-    handleClickHelpM(){
-        alert(this.state.helpM);
-    }
+    handleServiceOpen = () => this.setState({ modalSOpen: true })
+    handleServiceClose = () => this.setState({ modalSOpen: false })
+    handleHelpOpen = () => this.setState({ modalAOpen: true })
+    handleHelpClose = () => this.setState({ modalAOpen: false })
 
     render(){
+        const aboutUsM = this.state.aboutUsM;
+        const reviewM = this.state.reviewM;
+
         return(
             <Grid relaxed='very' style={style}>
 
                 <Grid.Column floated='right' width={5}>
                     <List selection>
-                        <List.Item onClick={this.handleClickAboutUs}>
-                            <List.Content style={styleYellowT}>Sobre Nosotros</List.Content>
-                        </List.Item>
-                        <List.Item onClick={this.handleClickOurServM}>
-                            <List.Content style={styleYellowT}>Nuestros Servicios</List.Content>
-                        </List.Item>
-                        <List.Item onClick={this.handleClickReviewM}>
-                            <List.Content style={styleYellowT}>Reseñas</List.Content>
-                        </List.Item>
-                        <List.Item onClick={this.handleClickHelpM}>
-                            <List.Content style={styleYellowT}>Ayuda</List.Content>
-                        </List.Item>
+                        <Modal
+                            trigger={<List.Item>
+                                        <List.Content style={styleYellowT}>Sobre Nosotros</List.Content>
+                                    </List.Item>}
+                            header='Sobre Nosotros'
+                            content= {aboutUsM}
+                            actions={[{ key: 'Entendido', content: 'Entendido', positive: true }]}
+                            basic
+                        />
+
+                        <Modal
+                            trigger={<List.Item onClick={this.handleServiceOpen}>
+                                        <List.Content style={styleYellowT}>Nuestros Servicios</List.Content>
+                                    </List.Item>}
+                            open={this.state.modalSOpen}
+                            onClose={this.handleServiceClose}
+                            basic
+                            >
+                            <Modal.Header>Nuestros Servicios</Modal.Header>
+                            <Modal.Content>            
+                                <Modal.Description> 
+                                <h3>Servicios:</h3>
+                                <p>- Pedir taxi</p>
+                                <p>- Aceptar una solicitud de presta servicio de taxi</p>
+                                </Modal.Description>
+                            </Modal.Content>
+                            <Modal.Actions>
+                                <Button color='green' onClick={this.handleServiceClose}> Entendido </Button>
+                            </Modal.Actions>
+                        </Modal>
+
+                        <Modal
+                            trigger={<List.Item>
+                                        <List.Content style={styleYellowT}>Reseñas</List.Content>
+                                    </List.Item>}
+                            header='Reseñas'
+                            content= {reviewM}
+                            actions={[{ key: 'Entendido', content: 'Entendido', positive: true }]}
+                            basic
+                        />
+
+                        <Modal
+                            trigger={<List.Item onClick={this.handleHelpOpen}>
+                                        <List.Content style={styleYellowT}>Ayuda</List.Content>
+                                    </List.Item>}
+                            open={this.state.modalAOpen}
+                            onClose={this.handleHelpClose}
+                            basic
+                            >
+                            <Modal.Header>Ayuda</Modal.Header>
+                            <Modal.Content>            
+                                <Modal.Description> 
+                                <h3>Correos:</h3>
+                                <p>Juan felipe Gil  -  juan.felipe.gil@correounivalle.edu.co</p>
+                                <p>Melissa Fuentes  -  melissa.fuentes@correounivalle.edu.co</p>
+                                </Modal.Description>
+                            </Modal.Content>
+                            <Modal.Actions>
+                                <Button color='green' onClick={this.handleHelpClose}> Entendido </Button>
+                            </Modal.Actions>
+                        </Modal>
                     </List>
                 </Grid.Column>
 
